9dedfa3f519df2cbd759c7c0e0a25d201660cdc4
time1043 <1665434994@qq.com>
2024-03-09 11:56:52 +0800

    course模块的添加功能和重构

--- rust_web/ws/webservice/src/handlers/course.rs
+++ rust_web/ws/webservice/src/handlers/course.rs
@@ -1,24 +1,24 @@
 use crate::state::AppState;
 use crate::dbaccess::course::*;
 use crate::errors::MyError;
-use crate::models::course::Course;
+use crate::models::course::{CreateCourse, UpdateCourse};
 use actix_web::{web, HttpResponse};
 
 
-pub async fn new_course(
-    new_course: web::Json<Course>,
+pub async fn post_new_course(
     app_state: web::Data<AppState>,
+    new_course: web::Json<CreateCourse>,
 ) -> Result<HttpResponse, MyError> {
-    post_new_course_db(&app_state.db, new_course.into())
+    post_new_course_db(&app_state.db, new_course.try_into()?)
         .await
         .map(|course| HttpResponse::Ok().json(course))
 }
 
 pub async fn get_courses_for_teacher(
     app_state: web::Data<AppState>,
-    params: web::Path<(usize,)>, // xxxx/{teacher_id}
+    params: web::Path<i32>,
 ) -> Result<HttpResponse, MyError> {
-    let teacher_id = i32::try_from(params.0).unwrap();
+    let teacher_id = params.into_inner();
     get_courses_for_teacher_db(&app_state.db, teacher_id)
         .await
         .map(|courses| HttpResponse::Ok().json(courses))
@@ -26,11 +26,31 @@ pub async fn get_courses_for_teacher(
 
 pub async fn get_course_detail(
     app_state: web::Data<AppState>,
-    params: web::Path<(usize, usize)>, // xxxx/{teacher_id}/{course_id}
+    params: web::Path<(i32, i32)>,
 ) -> Result<HttpResponse, MyError> {
-    let teacher_id = i32::try_from(params.0).unwrap();
-    let course_id = i32::try_from(params.1).unwrap();
+    let (teacher_id, course_id) = params.into_inner();
     get_course_details_db(&app_state.db, teacher_id, course_id)
+        .await
+        .map(|course| HttpResponse::Ok().json(course))
+}
+
+pub async fn delete_course(
+    app_state: web::Data<AppState>,
+    params: web::Path<(i32, i32)>,
+) -> Result<HttpResponse, MyError> {
+    let (teacher_id, course_id) = params.into_inner();
+    delete_course_db(&app_state.db, teacher_id, course_id)
+        .await
+        .map(|resp| HttpResponse::Ok().json(resp))
+}
+
+pub async fn update_course_details(
+    app_state: web::Data<AppState>,
+    update_course: web::Json<UpdateCourse>,
+    params: web::Path<(i32, i32)>,
+) -> Result<HttpResponse, MyError> {
+    let (teacher_id, course_id) = params.into_inner();
+    update_course_details_db(&app_state.db, teacher_id, course_id, update_course.into())
         .await
         .map(|course| HttpResponse::Ok().json(course))
 }
@@ -38,7 +58,7 @@ pub async fn get_course_detail(
 #[cfg(test)]
 mod tests {
     use super::*;
-    use actix_web::{http::StatusCode, web};
+    use actix_web::{http::StatusCode, web, ResponseError};
     use dotenv::dotenv;
     use sqlx::postgres::PgPoolOptions;
     use std::env;
@@ -48,55 +68,140 @@ mod tests {
     #[actix_rt::test]
     async fn post_course_test() {
         dotenv().ok();
-        let databse_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set."); // 读取数据库连接信息
-        let db_pool = PgPoolOptions::new().connect(&databse_url).await.unwrap(); // 创建数据库连接池
+        let db_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set.");
+        let db_pool = PgPoolOptions::new().connect(&db_url).await.unwrap();
         let app_state: web::Data<AppState> = web::Data::new(AppState {
             health_check_response: "".to_string(),
             visit_count: Mutex::new(0),
             db: db_pool,
         });
 
-        let course = web::Json(Course {
+        let course = web::Json(CreateCourse {
             teacher_id: 1,
             name: "Test course".into(),
-            id: Some(7), // serial
-            time: None,
+            description: Some("This is a course".into()),
+            format: None,
+            structure: None,
+            duration: None,
+            price: None,
+            language: Some("English".into()),
+            level: Some("Beginner".into()),
         });
-        let resp = new_course(course, app_state).await.unwrap();
+        let resp = post_new_course(app_state, course).await.unwrap();
         assert_eq!(resp.status(), StatusCode::OK);
     }
 
     #[actix_rt::test]
     async fn get_all_courses_success() {
         dotenv().ok();
-        let databse_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set."); // 读取数据库连接信息
-        let db_pool = PgPoolOptions::new().connect(&databse_url).await.unwrap(); // 创建数据库连接池
+        let db_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set.");
+        let db_pool = PgPoolOptions::new().connect(&db_url).await.unwrap();
         let app_state: web::Data<AppState> = web::Data::new(AppState {
             health_check_response: "".to_string(),
             visit_count: Mutex::new(0),
             db: db_pool,
         });
 
-        let teacher_id: web::Path<(usize,)> = web::Path::from((1,));
-        let resp = get_courses_for_teacher(app_state, teacher_id)
-            .await
-            .unwrap();
+        let teacher_id: web::Path<i32> = web::Path::from(1);
+        let resp = get_courses_for_teacher(app_state, teacher_id).await.unwrap();
         assert_eq!(resp.status(), StatusCode::OK);
     }
 
     #[actix_rt::test]
     async fn get_one_course_success() {
         dotenv().ok();
-        let databse_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set."); // 读取数据库连接信息
-        let db_pool = PgPoolOptions::new().connect(&databse_url).await.unwrap(); // 创建数据库连接池
+        let db_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set.");
+        let db_pool = PgPoolOptions::new().connect(&db_url).await.unwrap();
         let app_state: web::Data<AppState> = web::Data::new(AppState {
             health_check_response: "".to_string(),
             visit_count: Mutex::new(0),
             db: db_pool,
         });
 
-        let params: web::Path<(usize, usize)> = web::Path::from((1, 1));
+        let params: web::Path<(i32, i32)> = web::Path::from((1, 1));
         let resp = get_course_detail(app_state, params).await.unwrap();
         assert_eq!(resp.status(), StatusCode::OK);
+    }
+
+    #[actix_rt::test]
+    async fn get_one_course_failure() {
+        dotenv().ok();
+        let db_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set.");
+        let db_pool = PgPoolOptions::new().connect(&db_url).await.unwrap();
+        let app_state: web::Data<AppState> = web::Data::new(AppState {
+            health_check_response: "".to_string(),
+            visit_count: Mutex::new(0),
+            db: db_pool,
+        });
+
+        let params: web::Path<(i32, i32)> = web::Path::from((1, 100));
+        let resp = get_course_detail(app_state, params).await;
+        match resp {
+            Ok(_) => println!("Something wrong ..."),
+            Err(err) => assert_eq!(err.status_code(), StatusCode::NOT_FOUND),
+        }
+    }
+
+    #[actix_rt::test]
+    async fn update_one_course_success() {
+        dotenv().ok();
+        let db_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set.");
+        let db_pool = PgPoolOptions::new().connect(&db_url).await.unwrap();
+        let app_state: web::Data<AppState> = web::Data::new(AppState {
+            health_check_response: "".to_string(),
+            visit_count: Mutex::new(0),
+            db: db_pool,
+        });
+
+        let update_course = UpdateCourse {
+            name: Some("Course name changed".into()),
+            description: Some("This is anther test course".into()),
+            format: None,
+            level: Some("Intermediate".into()),
+            price: None,
+            duration: None,
+            language: Some("Chinese".into()),
+            structure: None,
+        };
+        let params: web::Path<(i32, i32)> = web::Path::from((1, 2));
+        let update_param = web::Json(update_course);
+        let resp = update_course_details(app_state, update_param, params).await.unwrap();
+        assert_eq!(resp.status(), StatusCode::OK);
+    }
+
+    #[ignore]
+    #[actix_rt::test]
+    async fn delete_course_success() {
+        dotenv().ok();
+        let db_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set.");
+        let db_pool = PgPoolOptions::new().connect(&db_url).await.unwrap();
+        let app_state: web::Data<AppState> = web::Data::new(AppState {
+            health_check_response: "".to_string(),
+            visit_count: Mutex::new(0),
+            db: db_pool,
+        });
+
+        let params: web::Path<(i32, i32)> = web::Path::from((1, 3));
+        let resp = delete_course(app_state, params).await.unwrap();
+        assert_eq!(resp.status(), StatusCode::OK);
+    }
+
+    #[actix_rt::test]
+    async fn delete_course_failure() {
+        dotenv().ok();
+        let db_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set.");
+        let db_pool = PgPoolOptions::new().connect(&db_url).await.unwrap();
+        let app_state: web::Data<AppState> = web::Data::new(AppState {
+            health_check_response: "".to_string(),
+            visit_count: Mutex::new(0),
+            db: db_pool,
+        });
+
+        let params: web::Path<(i32, i32)> = web::Path::from((1, 101));
+        let resp = delete_course(app_state, params).await;
+        match resp {
+            Ok(_) => println!("Something wrong ..."),
+            Err(err) => assert_eq!(err.status_code(), StatusCode::NOT_FOUND),
+        }
     }
 }
--- rust_web/ws/webservice/src/models/course.rs
+++ rust_web/ws/webservice/src/models/course.rs
@@ -1,10 +1,14 @@
+use std::convert::TryFrom;
+
 use actix_web::web;
 use chrono::NaiveDateTime;
 use serde::{Deserialize, Serialize};
 
+use crate::errors::MyError;
 
-// 仅作从数据库读取 不需要逆序列化
-#[derive(Serialize, Debug, Clone, sqlx::FromRow)]
+
+// 仅作从数据库读取 不需要逆序列化  (sqlx::FromRow 自动映射 不需要实现From)
+#[derive(Serialize, Deserialize, Debug, Clone, sqlx::FromRow)]
 pub struct Course {
     pub teacher_id: i32,
     pub id: i32,
@@ -19,7 +23,7 @@ pub struct Course {
     pub level: Option<String>,
 }
 
-// 有新增功能
+// 新增课程
 #[derive(Deserialize, Debug, Clone)]
 pub struct CreateCourse {
     pub teacher_id: i32,
@@ -33,13 +37,64 @@ pub struct CreateCourse {
     pub level: Option<String>,
 }
 
-impl From<web::Json<Course>> for Course {
-    fn from(course: web::Json<Course>) -> Self {
-        Course {
+/*impl From<web::Json<CreateCourse>> for CreateCourse {
+    fn from(course: web::Json<CreateCourse>) -> Self {
+        CreateCourse {
             teacher_id: course.teacher_id,
-            id: course.id,
             name: course.name.clone(),
-            time: course.time,
+            description: course.description.clone(),
+            format: course.format.clone(),
+            structure: course.structure.clone(),
+            duration: course.duration.clone(),
+            price: course.price,
+            language: course.language.clone(),
+            level: course.level.clone(),
+        }
+    }
+}*/
+
+impl TryFrom<web::Json<CreateCourse>> for CreateCourse {
+    type Error = MyError;
+
+    fn try_from(course: web::Json<CreateCourse>) -> Result<Self, Self::Error> {
+        Ok(CreateCourse {
+            teacher_id: course.teacher_id,
+            name: course.name.clone(),
+            description: course.description.clone(),
+            format: course.format.clone(),
+            structure: course.structure.clone(),
+            duration: course.duration.clone(),
+            price: course.price,
+            language: course.language.clone(),
+            level: course.level.clone(),
+        })
+    }
+}
+
+// 修改课程
+#[derive(Deserialize, Debug, Clone)]
+pub struct UpdateCourse {
+    pub name: Option<String>,
+    pub description: Option<String>,
+    pub format: Option<String>,
+    pub structure: Option<String>,
+    pub duration: Option<String>,
+    pub price: Option<i32>,
+    pub language: Option<String>,
+    pub level: Option<String>,
+}
+
+impl From<web::Json<UpdateCourse>> for UpdateCourse {
+    fn from(course: web::Json<UpdateCourse>) -> Self {
+        UpdateCourse {
+            name: course.name.clone(),
+            description: course.description.clone(),
+            format: course.format.clone(),
+            structure: course.structure.clone(),
+            duration: course.duration.clone(),
+            price: course.price,
+            language: course.language.clone(),
+            level: course.level.clone(),
         }
     }
 }
